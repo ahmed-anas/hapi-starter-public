dd5cc177a4b74e0694ca9fd2b9527bf279a47da1
Ahmed Anas <ahmed.a.tamim@gmail.com>
2020-06-25 16:53:25 +0500

    docker unit-test volume share removed

--- docker/docker-test.entrypoint.sh
+++ docker/docker-test.entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-cd /usr/src
-cd app
-
+cd /usr/src/app
 if [ "$USER" == "jenkins" ]
 then
     echo "Running unit tests in Jenkins without watch"
-    npm run unit-test -- --watch
+    npm run unit-test
 else
     echo "Running unit tests with watch"
-    npm run unit-test
+    npm run unit-test -- --watch
 fi
+
--- test.Dockerfile
+++ test.Dockerfile
@@ -1,9 +1,10 @@
 FROM node:12
 
-WORKDIR /usr/src/
+WORKDIR /usr/src/app
+ENV NODE_ENV development
 COPY ./package*.json ./
 COPY ./docker/docker-test.entrypoint.sh ./
+COPY ./ ./
 RUN npm install
-
 RUN ["chmod", "+x", "./docker-test.entrypoint.sh"]
 ENTRYPOINT ["./docker-test.entrypoint.sh"]
